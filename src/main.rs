7affe0a121a1bb15a5cf1df3d106d85725820332
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-05-21 11:56:43 +0530

    Class implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+
+
+impl Rectangle{
+
+    fn new(width:u32,height:u32)->Rectangle{
+        Rectangle{
+            width,
+            height,
+        }
+    }
+
+
+    fn area(&self)->u32{
+        self.width*self.height
+    }
+
+    fn perimeter(&self) -> u32 {
+        2 * (self.width + self.height)
+    }
+}
+
+
 fn main() {
-    let x: i32 = 5;
-    let y: i32 = 10;
-    let z: i32 = x + y;
-    print!("{}", z);
+    let rect = Rectangle::new(30,50);
+
+    println!("Area of rectangle is {}",rect.area());
 }
 
